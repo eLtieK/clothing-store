28695f7a347d63f9ede89677c10c8524c987b47e
eLtieK <kennezversion@gmail.com>
2024-12-06 19:26:45 +0700

    basic crud for order

--- backend/src/main/java/com/clothing_store/repository/ProductRepository.java
+++ backend/src/main/java/com/clothing_store/repository/ProductRepository.java
@@ -1,9 +1,8 @@
 package com.clothing_store.repository;
 
-import com.clothing_store.dto.response.ProductResponse;
-import com.clothing_store.entity.product.Product;
-import com.clothing_store.entity.user.User;
+import com.clothing_store.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -32,11 +31,23 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     void insertProductSize(@Param("p_product_id") String product_id,
                            @Param("p_size") String size);
 
-    @Procedure(procedureName = "GetAllProducts")
-    List<Object> getAllProducts();
+    @Query(value = "SELECT * FROM product", nativeQuery = true)
+    List<Product> getAllProducts();
 
-    @Procedure(procedureName = "GetProductById")
-    List<Object> getProductById(@Param("p_product_id") String product_id);
+    @Query(value = "SELECT * FROM product WHERE product_id = :productId", nativeQuery = true)
+    Product getProductById(@Param("productId") String productId);
+
+    @Query(value = "SELECT * FROM product_color", nativeQuery = true)
+    List<Object> getAllProductColors();
+
+    @Query(value = "SELECT * FROM product_color WHERE product_id = :productId", nativeQuery = true)
+    List<Object> getProductColorById(@Param("productId") String productId);
+
+    @Query(value = "SELECT * FROM product_size", nativeQuery = true)
+    List<Object> getAllProductSizes();
+
+    @Query(value = "SELECT * FROM product_size WHERE product_id = :productId", nativeQuery = true)
+    List<Object> getProductSizeById(@Param("productId") String productId);
 
     @Procedure(procedureName = "DeleteProduct")
     void deleteProduct(@Param("p_product_id") String product_id);
--- backend/src/main/java/com/clothing_store/service/ProductService.java
+++ backend/src/main/java/com/clothing_store/service/ProductService.java
@@ -2,17 +2,16 @@ package com.clothing_store.service;
 
 import com.clothing_store.dto.request.insert.ProductRequest;
 import com.clothing_store.dto.request.update.ProductUpdateRequest;
-import com.clothing_store.dto.response.ProductResponse;
-import com.clothing_store.entity.product.Product;
+import com.clothing_store.dto.response.ProductColorResponse;
+import com.clothing_store.dto.response.ProductSizeResponse;
+import com.clothing_store.entity.Product;
 import com.clothing_store.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ProductService {
@@ -43,29 +42,61 @@ public class ProductService {
     }
 
     @Transactional
-    public List<ProductResponse> getProducts() {
-        List<Object> raw_list = productRepository.getAllProducts();
-        List<ProductResponse> product_list = new ArrayList<>();
-
-        for (Object raw_data : raw_list) {
-            ProductResponse product_data = ProductResponse.convertToProductResponse(raw_data);
-            product_list.add(product_data);
-        }
-
-        return product_list;
+    public List<Product> getProducts() {
+        return productRepository.getAllProducts();
     }
 
     @Transactional
-    public List<ProductResponse> getProduct(String productId) {
-        List<Object> raw_list = productRepository.getProductById(productId);
-        List<ProductResponse> product_list = new ArrayList<>();
+    public Product getProduct(String productId) {
+        return productRepository.getProductById(productId);
+    }
+
+    @Transactional
+    public List<ProductColorResponse> getProductColors() {
+        List<Object> raw_list = productRepository.getAllProductColors();
+        List<ProductColorResponse> data_list = new ArrayList<>();
 
         for (Object raw_data : raw_list) {
-            ProductResponse product_data = ProductResponse.convertToProductResponse(raw_data);
-            product_list.add(product_data);
+            ProductColorResponse data = ProductColorResponse.convertToProductColorResponse(raw_data);
+            data_list.add(data);
         }
+        return data_list;
+    }
 
-        return product_list;
+    @Transactional
+    public List<ProductColorResponse> getProductColor(String productId) {
+        List<Object> raw_list = productRepository.getProductColorById(productId);
+        List<ProductColorResponse> data_list = new ArrayList<>();
+
+        for (Object raw_data : raw_list) {
+            ProductColorResponse data = ProductColorResponse.convertToProductColorResponse(raw_data);
+            data_list.add(data);
+        }
+        return data_list;
+    }
+
+    @Transactional
+    public List<ProductSizeResponse> getProductSizes() {
+        List<Object> raw_list = productRepository.getAllProductSizes();
+        List<ProductSizeResponse> data_list = new ArrayList<>();
+
+        for (Object raw_data : raw_list) {
+            ProductSizeResponse data = ProductSizeResponse.convertToProductSizeResponse(raw_data);
+            data_list.add(data);
+        }
+        return data_list;
+    }
+
+    @Transactional
+    public List<ProductSizeResponse> getProductSize(String productId) {
+        List<Object> raw_list = productRepository.getProductSizeById(productId);
+        List<ProductSizeResponse> data_list = new ArrayList<>();
+
+        for (Object raw_data : raw_list) {
+            ProductSizeResponse data = ProductSizeResponse.convertToProductSizeResponse(raw_data);
+            data_list.add(data);
+        }
+        return data_list;
     }
 
     @Transactional
